5b34367a82ee9ada2f63947f848b6a8a84894fcb
andigandhi <andigrasser13@gmail.com>
2021-11-21 13:14:07 +0000

    Deploying to gh-pages from master @ 686a00acaef6bb63802cd7c43cb43339b48e7ac9 🚀

--- jzScript.js
+++ jzScript.js
@@ -1,8 +1,3 @@
-
-//WA.onInit().then(() => {
-//	WA.chat.sendChatMessage('Servus '+WA.player.name+'! Viel Spaß im digitalen Jugge und schöne Weihnachten! :)', 'andigandhi');
-//});
-
 let GuMoPopup;
 
 // Begrüßungsnachricht
@@ -16,49 +11,13 @@ GuMoPopup = WA.onInit().then(() => {
 
 	setTimeout(() => {
 		triggerMessage.remove();
-	}, 2000)
+	}, 5000)
 });
 
 
 
-
-
-
-//https://andigandhi.ga/content/jz/audio.php
-var jzMusic = WA.sound.loadSound("https://andigandhi.ga/content/jz/audio.php");
-
-var loudMusic = {
-    volume : 0.8,
-    loop : false,
-    rate : 1,
-    detune : 1,
-    delay : 0,
-    seek : 0,
-    mute : false
-}
-
-var silentMusic = {
-    volume : 0.2,
-    loop : false,
-    rate : 1,
-    detune : 1,
-    delay : 0,
-    seek : 0,
-    mute : false
-}
+var jzMusic = WA.sound.loadSound("sound/outside.mp3");
 
 WA.onInit().then(() => {
-    jzMusic.play(loudMusic);
-	WA.chat.sendChatMessage('init', 'andigandhi');
+    jzMusic.play();
 })
-
-WA.room.onEnterZone('music', () => {
-    jzMusic.play(loudMusic);
-	WA.chat.sendChatMessage('enter zone', 'andigandhi');
-})
-
-WA.room.onLeaveZone('music', () => {
-    jzMusic.play(silentMusic);
-	WA.chat.sendChatMessage('exit zone', 'andigandhi');
-})
-
