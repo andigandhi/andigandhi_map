29bbdd5308feb139b68e6c166baa1d64c3184dd8
andigandhi <andigrasser13@gmail.com>
2021-11-21 12:20:11 +0100

    Update

--- jzScript.js
+++ jzScript.js
@@ -2,3 +2,22 @@
 //WA.onInit().then(() => {
 //	WA.chat.sendChatMessage('Servus '+WA.player.name+'! Viel Spaß im digitalen Jugge und schöne Weihnachten! :)', 'andigandhi');
 //});
+
+let GuMoPopup;
+
+// Open the popup when we enter a given zone
+GuMoPopup = WA.room.onEnterZone('start', () => {
+    WA.ui.openPopup("start-popup", 'Hallo '+WA.player.name+', schön dass du da bist!', [{
+        label: "OK",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    });
+}]);
+
+// Close the popup when we leave the zone.
+WA.room.onLeaveZone('start', () => {
+    GuMoPopup.close();
+});
