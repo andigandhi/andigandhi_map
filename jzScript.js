d26b3ecf4adc6f274caf22fd275bd97569fcd8e5
andigandhi <andigrasser13@gmail.com>
2021-11-21 10:32:36 +0000

    Deploying to gh-pages from master @ f29363633091a0f14b443c1afc7da544343f7db6 🚀

--- jzScript.js
+++ jzScript.js
@@ -1,4 +1,4 @@
 
-WA.onInit().then(() => {
-	WA.chat.sendChatMessage('Servus '+WA.player.name+'! Viel Spaß im digitalen Jugge und schöne Weihnachten! :)', 'andigandhi');
-});
+//WA.onInit().then(() => {
+//	WA.chat.sendChatMessage('Servus '+WA.player.name+'! Viel Spaß im digitalen Jugge und schöne Weihnachten! :)', 'andigandhi');
+//});
